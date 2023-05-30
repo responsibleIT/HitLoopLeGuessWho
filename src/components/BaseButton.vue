f9a1239074a8a6b7573d9d95c127a5f0306f6976
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-05-30 14:15:10 +0200

    initial commit

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  name: String
+})
+</script>
+
+
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from 'vue';
+import BaseButton from '../components/BaseButton.vue';
+import TheWelcome from '../components/TheWelcome.vue';
+
+let data = ref('hallo')
+
 </script>
 
 <template>
   <main>
+    <BaseButton>{{ name }}</BaseButton>
     <TheWelcome />
+    
   </main>
 </template>
