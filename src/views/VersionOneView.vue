151141a5804cbaf569490ed68026ab8a57f8c8c3
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-05-30 22:08:00 +0200

    removed unused stuff & added play pause button

--- src/views/VersionOneView.vue
+++ src/views/VersionOneView.vue
@@ -1,17 +1,29 @@
 <script setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
 import BaseButton from '../components/BaseButton.vue';
 import BeatVersionOne from '@/components/BeatVersionOne.vue';
+import { useToggle } from '@vueuse/core'
 
 let data = ref('hallo')
-
-
+const playPause = ref(false);
+const togglePlayPause = useToggle(playPause)
 
 </script>
 
 <template>
-  <main>
-    <BaseButton>{{ data }}</BaseButton>
-<BeatVersionOne/>
-  </main>
+<main>
+  <BeatVersionOne />
+  <div>{{ playPause }}</div>
+  <div>
+    <button v-if="!playPause" @click="togglePlayPause()">play</button>
+    <button v-else @click="togglePlayPause()">pause</button>
+  </div>
+</main>
 </template>
+a
+<style scoped>
+main {
+  margin-inline: auto;
+
+}
+</style> 
