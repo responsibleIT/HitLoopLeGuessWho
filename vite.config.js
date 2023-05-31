00bcd16a126805f4ed4ce69fccd13168812dc738
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-05-31 14:56:57 +0200

    add css sourceMaps

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     port: 3000,
     hmr: true
   },
+  css: {
+    devSourcemap: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
