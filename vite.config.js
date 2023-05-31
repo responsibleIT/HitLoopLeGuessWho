a273cb59d44a5f7fc4ec6e6cb8d35fd1796c6c0a
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-05-31 14:55:55 +0200

    dev port to 3000

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000,
+    hmr: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
