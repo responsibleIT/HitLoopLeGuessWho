bebd06657414a91f3f3b757ed632801da9094e06
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-05-31 14:50:08 +0200

    add pwa images

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,62 @@
+<script setup>
+// defineProps = ({
+//   name: String,
+//   fill: Number,
+//   weight: Number,
+//   opsz: Number
+// });
+
+defineProps({
+  name: String,
+  FILL: {
+    type: Number,
+    default: 1,
+  },
+  wght: {
+    type: [Number, String],
+    default: 400,
+  },
+  GRAD: {
+    type: Number,
+    default: 200,
+  },
+  opsz: {
+    type: Number,
+    default: 48,
+  },
+});
+</script>
+
+<template>
+  <span class="material-symbols-rounded">
+    {{ name }}
+  </span>
+</template>
+
+<style lang="scss" scoped>
+span {
+  font-size: inherit;
+}
+
+.icon {
+  // font-size: inherit;
+}
+span.icon:hover {
+  // transform: scale(2);
+}
+// .material-symbols-rounded {
+//   font-variation-settings:
+//   'FILL' 1,
+//   'wght' 500,
+//   'GRAD' 0,
+//   'opsz' 24
+// }
+
+.material-symbols-rounded {
+  // font-size: 1em;
+  font-size: inherit;
+
+  font-variation-settings: "FILL" v-bind(FILL), "wght" v-bind(wght), "GRAD" v-bind(GRAD),
+    "opsz" v-bind(opsz);
+}
+</style>
