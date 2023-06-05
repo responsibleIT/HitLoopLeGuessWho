9bfc23cc1e2caa95b5f2205fd472054c094c372b
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-06-05 16:24:19 +0200

    commited for upcoming changes

--- src/views/VersionOneView.vue
+++ src/views/VersionOneView.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue'
 import BeatVersionOne from '@/components/BeatVersionOne.vue'
 import { useToggle } from '@vueuse/core'
 import StepSequencer from '@/components/StepSequencer.vue'
+import StepSequencerVersionTwo from '../components/StepSequencerVersionTwo.vue';
 
 const playPause = ref(false)
 // const togglePlayPause = useToggle(playPause)
@@ -12,7 +13,8 @@ const playPause = ref(false)
 <template>
   <main>
     <!-- <StepSequencer /> -->
-    <BeatVersionOne/>
+    <!-- <BeatVersionOne/> -->
+    <StepSequencerVersionTwo/>
 
     <!-- <div>{{ playPause }}</div>
   <div>
