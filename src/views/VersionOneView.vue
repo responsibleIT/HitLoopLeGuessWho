2425d72474b013c933843c2e78bb497ab19b0314
wongsrila <jiraphong.wongsrila@gmail.com>
2023-06-06 11:17:19 +0200

    cleaned errors on main component

--- src/views/VersionOneView.vue
+++ src/views/VersionOneView.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
-import BeatVersionOne from '@/components/BeatVersionOne.vue'
-import { useToggle } from '@vueuse/core'
-import StepSequencer from '@/components/StepSequencer.vue'
-import StepSequencerVersionTwo from '../components/StepSequencerVersionTwo.vue';
+// import BeatVersionOne from '@/components/BeatVersionOne.vue'
+// import { useToggle } from '@vueuse/core'
+// import StepSequencer from '@/components/StepSequencer.vue'
+import StepSequencerVersionTwo from '../components/StepSequencerVersionTwo.vue'
 
-const playPause = ref(false)
+// const playPause = ref(false)
 // const togglePlayPause = useToggle(playPause)
 </script>
 
@@ -14,7 +14,7 @@ const playPause = ref(false)
   <main>
     <!-- <StepSequencer /> -->
     <!-- <BeatVersionOne/> -->
-    <StepSequencerVersionTwo/>
+    <StepSequencerVersionTwo />
 
     <!-- <div>{{ playPause }}</div>
   <div>
