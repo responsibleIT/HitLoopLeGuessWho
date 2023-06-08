9c46d22d05522adb5136084fa7d4c6c27084f136
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-06-08 14:09:02 +0200

    sequencer state management

---
+++ src/stores/sequence.js
@@ -0,0 +1,16 @@
+import { ref, computed, reactive } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useSequenceStore = defineStore('sequence', () => {
+  const bpm = ref(130);
+  const state = reactive({
+    bpm: 130,
+  })
+  const count = ref(0)
+  const doubleCount = computed(() => count.value * 2)
+  function increment() {
+    count.value++
+  }
+
+  return { count, doubleCount, increment, bpm, state }
+})
