9b2b22b1157e3a202811b393f7a52da798694a50
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-06-09 17:06:15 +0200

    added dynamic button component

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,13 +1,40 @@
 <script setup>
+import { useAttrs } from 'vue'
+import BaseIcon from './BaseIcon.vue'
+
 defineProps({
-  name: String
+  name: String,
+  icon: String
 })
 </script>
 
 <template>
-  <button>
+  <button class="btn-icon" v-if="icon">
+    <BaseIcon class="icon" :name="icon" />
+  </button>
+  <button class="btn" v-else>
     <slot></slot>
   </button>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.btn-icon {
+  font-size: 1.5em;
+  width: 4rem;
+  height:4rem;
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  text-align: center;
+  justify-content: center;
+
+  span {
+    text-align: center;
+    font-size: 2em;
+    // display: block;
+    align-self: center;
+  }
+}
+
+
+</style>
