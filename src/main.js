f24288bb0458063aa4073dd699df605c4a095a6f
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-06-18 18:04:36 +0200

    switched router and pinia

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import App from './App.vue'
 import router from './router/index.js'
 
 const app = createApp(App)
-
-app.use(createPinia())
 app.use(router)
+app.use(createPinia())
+
 
 app.mount('#app')
