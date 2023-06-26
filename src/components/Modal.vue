40cd1c44030dbe3c8b72f315cf2663c71d51ed53
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-06-26 10:38:15 +0200

    made files prettier

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,14 +1,7 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <div>
-
-  </div>
+  <div></div>
 </template>
 
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
